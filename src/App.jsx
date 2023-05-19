b4ff3627cbd48c3f4e2a1ee165027c57027a1d46
Yoel-Gasca <83617933+Yoel-Gasca@users.noreply.github.com>
2023-05-18 21:31:48 -0600

    Layout and logical operation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,48 @@
+import { useState } from "react"
+
 const TURNS = {
   X: 'X',
   O: 'O'
 }
+// --Funcionamiento del tablero--
 
-const board = Array(9).fill(null)
+//Funciones para los turnos
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  // Condicion para los turnos
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  //
+  const handleClick = () => {
+    updateBoard(index)
+  }
+
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  )
+}
 
 function App() {
+  // Cuadricula del Tablero
+  const [board, setBoard] = useState(Array(9).fill(null))
+
+  // Estado para los turnos
+  const [turn, setTurn] = useState(TURNS.X)
+
+  const updateBoard = (index) =>{
+    // No actualizar posicion si esta ocupada
+    if(board[index])return
+    //Atualiza el Board
+    const newBoard = [...board]
+    newBoard[index]= turn
+    setBoard(newBoard)
+
+    //Condicion para cambiar turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+  }
+
   return (
     <main className='board'>
       <h1>Juego del Gato</h1>
@@ -13,14 +50,25 @@ function App() {
         {
           board.map((_, index) =>{
             return (
-              <div className="cell" key={index}>
-                <span className="cell_content">
-                  {index}
-                </span>
-              </div>
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
+                {board[index]}
+              </Square>
             )
           })
         }
+      </section>
+      
+      <section className="turn">
+        <Square isSelected= {turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected= {turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
       </section>
     </main>
   ) 
